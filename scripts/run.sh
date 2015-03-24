98d2ce34180991ca4633956e9bbf831b28002fb6
imanolgo <yo@imanolgomez.net>
2015-03-24 15:56:36 +0100

    Solved run.sh script problem. Now both software start simultaniously

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,14 +11,11 @@ echo '-> Halo Light Script'
 echo '-> Starting Fade Candy Server'
 
 cd ../fcserver
-./fcserver-osx ServerConfig.json 
+./fcserver-osx ServerConfig.json &
 
-echo '-> waiting...'
-
-sleep 10
 
 echo '-> Starting Halo Light Application'
 
 cd ../bin
-open HaloLight.app
+open HaloLight.app &
 
